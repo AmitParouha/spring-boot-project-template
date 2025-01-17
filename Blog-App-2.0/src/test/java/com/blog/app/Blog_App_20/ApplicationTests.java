6ba8528fa34e2f89ccb3d83d0a6fda8b4aeba801
Amit <amitparouha010@gmail.com>
2025-01-18 00:45:49 +0530

    'Blog-APP'

---
+++ Blog-App-2.0/src/test/java/com/blog/app/Blog_App_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.app.Blog_App_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
